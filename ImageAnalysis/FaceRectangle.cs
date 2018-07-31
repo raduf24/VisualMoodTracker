09f14d37ad31b2021c601700e1f34772c1ad45a1
Barna Barabasi <barabasibarna@gmail.com>
2018-07-31 14:13:20 +0300

    Image Analysis Library ready for testing

---
+++ ImageAnalysis/FaceRectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ImageAnalysis
+{
+    public class FaceRectangle
+    {
+        public int top { get; }
+        public int left { get; }
+        public int width { get; }
+        public int height { get; }
+
+        public FaceRectangle(int top, int left, int width, int height)
+        {
+            this.top = top;
+            this.left = left;
+            this.width = width;
+            this.height = height;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Top: {0} \n Left: {1} \n Width: {2} \n Height: {3} \n", this.top, this.left, this.width, this.height);
+        }
+    }
+}
