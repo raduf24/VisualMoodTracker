f674b0c865fbe4220f064e7ff97c6b114aeda4e1
Hunor Czirjek <hczirjek@endava.com>
2018-09-28 07:51:46 +0300

    API endpoint and key changed to free trial (7 days)

--- VisualMoodTracker/ImageAnalysis/ImageAnalyze.cs
+++ VisualMoodTracker/ImageAnalysis/ImageAnalyze.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Net.Http.Headers;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
-using ImageAnalysis.Properties;
-using System.Configuration;
-using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ImageAnalysis
 {
